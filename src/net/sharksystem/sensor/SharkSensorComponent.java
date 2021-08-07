ab48a2301b7df29433fa85c0ab5d1ad526dfb5c6
Amy Keks <j.illbruck@gmail.com>
2021-08-07 15:36:16 +0200

    Adjust interface for SharkSensorComponent

--- src/net/sharksystem/sensor/SharkSensorComponent.java
+++ src/net/sharksystem/sensor/SharkSensorComponent.java
@@ -3,11 +3,23 @@ package net.sharksystem.sensor;
 import net.sharksystem.ASAPFormats;
 import net.sharksystem.SharkComponent;
 
-import java.io.File;
+import java.util.Date;
+import java.util.List;
 
 @ASAPFormats(formats = {SharkSensorComponent.APP_FORMAT})
 public interface SharkSensorComponent extends SharkComponent {
     String APP_FORMAT = "shark/sensor";
+    String URI = "sharkSensor://sensorData";
 
-    void readSensorDataFromFile(File dataFile);
-}
+
+    List<SensorData> getSensorDataForId(String id);
+
+    SensorData getSensorDataForIdAndTime(String id, Date dt);
+
+    void checkForNewDataInDB();
+
+    void sendNewSensorDataToASAPPeer(SensorData sensorData);
+
+
+
+    }
