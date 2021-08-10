d69c7b095deb20a64c1a528d6f769e333f5a8bac
Amy Keks <j.illbruck@gmail.com>
2021-08-10 12:18:09 +0200

    Adjust interface for SharkSensorComponent

--- src/net/sharksystem/sensor/SharkSensorComponentFactory.java
+++ src/net/sharksystem/sensor/SharkSensorComponentFactory.java
@@ -3,9 +3,20 @@ package net.sharksystem.sensor;
 import net.sharksystem.SharkComponent;
 import net.sharksystem.SharkComponentFactory;
 
+
 public class SharkSensorComponentFactory implements SharkComponentFactory {
+
+    private final SharkSensorSerializer serializer;
+    private final SensorRepository repo;
+
+    public SharkSensorComponentFactory(SensorRepository repo, SharkSensorSerializer serializer){
+        this.repo = repo;
+        this.serializer = serializer;
+    }
+
     @Override
     public SharkComponent getComponent() {
-        return new SharkSensorComponentImpl();
+        return new SharkSensorComponentImpl(
+                this.repo, this.serializer);
     }
 }
--- src/net/sharksystem/sensor/SharkSensorComponentImpl.java
+++ src/net/sharksystem/sensor/SharkSensorComponentImpl.java
@@ -1,18 +1,35 @@
 package net.sharksystem.sensor;
 
-import net.sharksystem.SharkComponent;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import net.sharksystem.SharkException;
 import net.sharksystem.SharkUnknownBehaviourException;
-import net.sharksystem.asap.ASAPException;
-import net.sharksystem.asap.ASAPPeer;
+import net.sharksystem.asap.*;
 
-import java.io.File;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 
-public class SharkSensorComponentImpl implements SharkSensorComponent {
+public class SharkSensorComponentImpl implements SharkSensorComponent, ASAPMessageReceivedListener {
+
+    private SensorRepository repo;
+    private ASAPPeer peer;
+    private SharkSensorSerializer serializer;
+
+
     @Override
     public void onStart(ASAPPeer asapPeer) throws SharkException {
-        // TODO
+        this.peer = asapPeer;
+
+        this.peer.addASAPMessageReceivedListener(
+                SharkSensorComponent.APP_FORMAT, this);
+    }
+
+    public SharkSensorComponentImpl(SensorRepository repo, SharkSensorSerializer serializer){
+        this.repo = repo;
+        this.serializer = serializer;
 
     }
 
@@ -21,7 +38,53 @@ public class SharkSensorComponentImpl implements SharkSensorComponent {
         // IGNORE
     }
 
-    public void readSensorDataFromFile(File dataFile) {
-        // TODO
+
+    @Override
+    public void checkForNewDataInDB(){
+        //TODO
+    }
+
+    @Override
+    public void sendNewSensorDataToASAPPeer(SensorData sensorData) {
+        try {
+            this.peer.sendASAPMessage(APP_FORMAT, URI, serializer.serializeSensorData(sensorData).getBytes());
+        }
+        catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        catch (NullPointerException e){
+            e.printStackTrace();
+        }
+        catch (ASAPException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<SensorData> getSensorDataForId(String id){
+        return repo.selectAllForId(id);
+    }
+
+    @Override
+    public SensorData getSensorDataForIdAndTime(String id, Date dt) {
+        return repo.selectSpecificEntry(id, dt);
+    }
+
+    @Override
+    public void asapMessagesReceived(ASAPMessages asapMessages, String s, List<ASAPHop> list) throws IOException {
+        CharSequence uri = asapMessages.getURI();
+
+        Iterator<byte[]> msgIter = asapMessages.getMessages();
+
+        if (URI.equals(uri.toString())) {
+            while (msgIter.hasNext()) {
+                byte[] msgContent = msgIter.next();
+                ByteArrayInputStream bais = new ByteArrayInputStream(msgContent);
+                DataInputStream dais = new DataInputStream(bais);
+
+                this.repo.insertNewEntries(serializer.deserializeSensorData(dais.readUTF()));
+
+            }
+        }
     }
 }
